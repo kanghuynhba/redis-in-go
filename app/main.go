ff97eda6f0df2fb17ef28043639c87753c1747cb
Khang Huỳnh Bá <kanghuynhba@gmail.com>
2026-08-28 12:10:27 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,30 +6,42 @@ import (
 	"os"
 )
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		_, err := conn.Read(buf)
+		if err != nil {
+			break
+		}
+		conn.Write([]byte("+PONG\r\n"))
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment the code below to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Cannot connect to client")
-		os.Exit(1)
-	}
-	buf := make([]byte, 1024)
-	for {
-		_, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-		conn.Write([]byte("+PONG\r\n"))
-	}
+	defer l.Close()
 
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Cannot connect to client")
+			continue
+		}
+
+		// Handle each connection concurrently in a separate goroutine
+		go handleConnection(conn)
+	}
 }
